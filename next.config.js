5cd9656bbdcfd0e9b6c399cd030b1431ec0586e7
Deni Kotsev <d.kotsev@netconomy.net>
2025-06-01 11:00:39 +0200

    play with cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'crossOrigin',
+            value: 'anonymous',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer-when-downgrade',
+          },
+        ],
+      },
+    ];
+  }
+};
 
 module.exports = nextConfig;
